139300c7f1ff0fbf662776800da8f1a14e195a9a
PaninNet <iliapanin300999@mail.ru>
2021-04-22 09:55:48 +0300

    Третья практическая

--- script/script.js
+++ script/script.js
@@ -8,32 +8,62 @@ function Client(count, movies, actors, genres,privat, name) {
 }
 
 let me = new Client(+prompt("Сколько фильмов вы уже посмотрели?"),{},{},[],false, prompt("Ваше имя"))
+let i = new Client(+prompt("Сколько фильмов вы уже посмотрели?"),{},{},[],false, prompt("Ваше имя"))
 
 Client.prototype.sayName = function () {
   return `My name is ${this.name}`
 };
-for (let i = 1; i <= 2; i++) {
-  let last = prompt("Последний ваш фильм")
-  let mark = prompt("Ваша оценка")
-  if (last === "" || mark === "" || last.length>50 || last === null || mark === null) {
-    i--
+Client.prototype.quize = function (){
+  for (let i = 1; i <= 2; i++) {
+    let last = prompt("Последний ваш фильм")
+    let mark = prompt("Ваша оценка")
+    if (last === "" || mark === "" || last.length>50 || last === null || mark === null) {
+      i--
+    }
+    else {
+      this.movies[last] = mark
+    }
+  }
+  return this.movies
+}
+Client.prototype.check = function () {
+  if (this.count<10){
+    alert("Too low")
+  }
+  else if (this.count > 10 && this.count <30) {
+    alert("normal")
+  }
+  else if (this.count > 30) {
+    alert("too much")
   }
   else {
-    me.movies[last] = mark
+    alert("smt strange")
   }
 }
-if (me.count<10){
-  alert("Too low")
+
+Client.prototype.priv = function () {
+  if (this.privat === false) {
+    console.log(this);
+  }
+  else {
+    console.log("true");
+  }
 }
-else if (me.count > 10 && me.count <30) {
-  alert("normal")
+
+Client.prototype.writeYourGenres = function () {
+  for (let i = 0; i < 3; i++) {
+    let favFilm = +prompt("Your favority film is ")
+    if (favFilm === ""|| isNaN(favFilm)=== true || favFilm < 0 || favFilm === 0) {
+      i--
+    }
+    else {
+      this.genres.push(favFilm)
+    }
+
+  }
+  return this.genres
 }
-else if (me.count > 30) {
-  alert("too much")
-}
-else {
-  alert("smt strange")
-}
+
 // let last = prompt("Последний ваш фильм")
 // let mark = prompt("Ваша оценка")
 // let llast = prompt("Последний ваш фильм")
