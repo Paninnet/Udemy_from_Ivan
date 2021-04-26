8d2256fef2addec725ee99d9fe83ebd3a5cae143
PaninNet <iliapanin300999@mail.ru>
2021-04-26 22:44:10 +0300

    Законченная практическая

--- script/pagescript.js
+++ script/pagescript.js
@@ -11,42 +11,110 @@
 Отсортировать их по алфавиту
 
 5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
+window.addEventListener("DOMContentLoaded" ,() =>{
+  const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против...",
+            "Апачи"
+        ]
+    };
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против...",
-        "Алф",
-        "Бва"
-    ]
-};
-
-let add = document.querySelector('.promo__adv')
-add.querySelectorAll('img').forEach((item) => {
-  item.remove()
-});
-add.querySelectorAll('div').forEach((item) => {
-  item.remove()
-});
-
-
-let text = document.querySelector('.promo__genre').innerHTML="Драма"
-let bg = document.querySelector('.promo__bg').classList.add("newbg")
+    const add = document.querySelectorAll('.promo__adv img')
+    const ul = document.querySelector('.promo__interactive-list')
+    const bd = movieDB.movies.sort()
+    const input = document.querySelector('.adding__input')
+    const btn =document.querySelector('button')
+    const error = document.querySelector('.filmName')
+    let check = document.querySelector('.check')
 
 
 
-let ul = document.querySelector('.promo__interactive-list')
-ul.innerHTML=""
 
-let mo = movieDB.movies
 
-mo.sort().forEach((item,i) => {
-  return  ul.insertAdjacentHTML('beforeend', `<li class="promo__interactive-item">${++i} ${item}
-                                                <div class="delete"></div>
-                                              </li>`)
-});
+    let changeAbit = () =>{
+      let changeTitle = document.querySelector('.promo__genre').innerHTML ="Драма"
+      let changeBg = document.querySelector('.promo__bg').classList.add("newbg")
+    }
+
+    let deleteAdd = (nodeArr) =>{
+      nodeArr.forEach((item) => {
+        item.remove()
+      });
+
+    }
+
+    let addFav =(checkbox,input) =>{
+      if (checkbox.checked && input.value !==""){
+        alert(`Добавляем ${input.value} в избранные`)
+        console.log(`Добавляем ${input.value} в избранные`);
+        checkbox.checked=false
+      }
+    }
+
+    const sortArr = (arr) => {
+     arr.sort();
+ };
+
+    let filmInOrder = (ul, bd) =>{
+      sortArr(bd)
+      ul.innerHTML=""
+      bd.forEach((item, i) => {
+        ul.innerHTML +=`<li class="promo__interactive-item"> ${i+1})${item}
+          <div class="delete"></div>
+        </li>`
+
+        document.querySelectorAll('.promo__interactive-item .delete').forEach((item,i) => {
+        item.addEventListener("click",(e)=>{
+          e.target.parentNode.remove()
+          bd.splice(i,1)
+          filmInOrder(ul, bd)
+        })
+
+      });
+      });
+
+    }
+
+
+
+    btn.addEventListener("click", (e)=>{
+      e.preventDefault()
+      if(input.value.length>21){
+        input.value = `${input.value.slice(1,21)}....`
+        movieDB.movies.push(input.value)
+        filmInOrder(ul, bd)
+        addFav(check,input)
+        error.style.color="black"
+      }
+      else if (input.value === "") {
+        error.style.color="red"
+      }
+      else{
+        movieDB.movies.push(input.value)
+        filmInOrder(ul, bd)
+        addFav(check,input)
+        error.style.color="black"
+
+      }
+      input.value=""
+    })
+
+
+
+
+
+
+
+
+    changeAbit()
+    deleteAdd(add)
+    filmInOrder(ul, bd)
+
+
+
+    });
